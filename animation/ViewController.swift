105b57b0fd543ee3bdeafca40844ad5b555678ba
Nova Arisma <novaarisma95@gmail.com>
2019-05-18 11:32:17 +0700

    17 mei 2019

--- animation/ViewController.swift
+++ animation/ViewController.swift
@@ -6,15 +6,22 @@
 //  Copyright © 2019 Nova Arisma. All rights reserved.
 //
 
+//bikin animasi
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewFrame: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
+        //time interval dalam detik
+        UIView.animate(withDuration: 2, animations:{
+            self.viewFrame.frame = CGRect(x: 80, y: 90, width: 100, height: 100)
+            
+        })
+///durasinya UIView.animate(withDuration: 2, animations:{
+///animation ukurannya self.viewFrame.frame = CGRect(x: 80, y: 90, width: 100, height: 100)
 }
 
+}
